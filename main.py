6c2b68b77e039119b1ecef730122e5a065c813b4
vijayshinde3004 <vijaymshinde96@gmail.com>
2023-06-12 18:11:09 +0530

    config entity and artifact entity code written

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 import os ,sys
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity import config_entity
 
 
 if __name__=="__main__":
     try:
-      get_collection_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
+      training_pipeline_config=config_entity.TrainingPipelineConfig()
+      data_ingestion_config=config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+      print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
