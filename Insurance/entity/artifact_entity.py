1228e0bec069e3fb545fca7a5281f7586f810e7c
vijayshinde3004 <vijaymshinde96@gmail.com>
2023-06-13 14:49:45 +0530

    model evaluation and model pusher complited

--- Insurance/entity/artifact_entity.py
+++ Insurance/entity/artifact_entity.py
@@ -22,3 +22,15 @@ class ModelTrainerArtifact:
     model_path: str    
     r2_train_score: float
     r2_test_score: float
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    improved_accuracy: float
+
+
+
+@dataclass
+class ModelPusherArtifact:
+    pusher_model_dir:str 
+    saved_model_dir:str
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
 from Insurance.components.data_transformation import DataTransformation
 from Insurance.components.model_trainer import ModelTrainer
-# from Insurance.components.model_evaluation import ModelEvaluation
-# from Insurance.components.model_pusher import ModelPusher
+from Insurance.components.model_evaluation import ModelEvaluation
+from Insurance.components.model_pusher import ModelPusher
 
 
 
@@ -44,6 +44,26 @@ if __name__ == "__main__":
         model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+
+        ## Model Evaluation
+
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+        model_eval = ModelEvaluation(model_eval_config = model_eval_config,
+        data_ingestion_artifact = data_ingestion_artifact,
+        data_transformation_artifact = data_transformation_artifact,
+        model_trainer_artifact = model_trainer_artifact)    
+        model_evaluation_artifact = model_eval.initiate_model_evaluation()   
+
+        # model pusher
+
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+                                     data_transformation_artifact=data_transformation_artifact,
+                                     model_trainer_artifact=model_trainer_artifact)
+        model_pusher_artifact = model_pusher.initiate_model_pusher()   
+
+
 
     except Exception as e:
         print(e)
